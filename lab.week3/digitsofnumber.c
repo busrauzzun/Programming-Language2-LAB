247f3d38810bf656b49993ffab7648fc12eb3607
busrauzzun <99530267+busrauzzun@users.noreply.github.com>
2022-02-24 11:00:25 +0300

    lab3

---
+++ lab.week3/digitsofnumber.c
@@ -0,0 +1,22 @@
+
+int main() {
+	
+	int n;
+	printf("Enter number");
+	scanf("%d",&n);
+	basamak(n);
+	printf("%d",basamak(n));
+	
+	return 0;
+}
+
+int basamak (int n){
+	
+	if(n>0){
+		
+		return (n%10+ basamak(n/10));
+		
+		
+	}
+	
+}
