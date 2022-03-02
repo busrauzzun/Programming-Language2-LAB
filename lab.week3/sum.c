2e66aab4c8dd8219ed6706a03a26fe4c5059eb89
busrauzzun <99530267+busrauzzun@users.noreply.github.com>
2022-03-02 23:21:51 +0300

    lab3

--- lab.week3/sum.c
+++ lab.week3/sum.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main() {
-	int n;
-	printf("Enter number\n");
-	scanf("%d",&n);
-	
-	sum(n);
-	printf("%d",sum(n));
-	return 0;
-}
-
 int sum(int n){
 	if(n>0){
 		return (n+sum(n-1));
 	}
 	
 }
+
+int main() {
+	int n;
+	printf("Enter number\n");
+	scanf("%d",&n);
+	
+	
+	printf("%d",sum(n));
+	return 0;
+}
+
