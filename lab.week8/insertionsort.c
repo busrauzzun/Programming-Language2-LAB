de9706c5287ff597e54c77a71e96c9a9a426a5a6
busrauzzun <99530267+busrauzzun@users.noreply.github.com>
2022-05-14 13:12:52 +0300

    lab8

--- lab.week8/insertionsort.c
+++ lab.week8/insertionsort.c
@@ -8,8 +8,7 @@ void insertionSort(int arr[], int n)
         key = arr[i];
         j = i - 1;
  
-       
-        while (j >= 0 && arr[j] > key) {
+       while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
